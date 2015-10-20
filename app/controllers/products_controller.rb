a6ef612f597b9151b36aae7353235f994a520d70
katherine <muedasnka@gmail.com>
2015-10-20 12:28:13 -0600

    create products list

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,6 @@
 class ProductsController < ApplicationController
   def index
+    # Create your list consisting of all active products to buy
+    smart_listing_create :products, Product.active, partial: "products/products_to_buy_list"
   end
 end
