446abfa9c7e2d97b81fb5da19fa7778e59689d02
katherine <muedasnka@gmail.com>
2015-10-20 23:40:14 -0600

    add sorting and better ux table

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,54 @@
 class ProductsController < ApplicationController
+  include SmartListing::Helper::ControllerExtensions
+  helper  SmartListing::Helper
+  
+  before_filter :find_product, except: [:index, :new, :create]
   def index
-    # Create your list consisting of all active products to buy
-    smart_listing_create :products, Product.active, partial: "products/products_to_buy_list"
+    @products = smart_listing_create(:products_to_buy, Product.all, partial: "products/products_to_buy_list", default_sort: {name: "asc"})
   end
+
+  def new
+    @product = Product.new
+  end
+  
+  def create
+    @product = Product.create(product_params)
+    # if @product.save
+    #   redirect_to products_path, notice: 'Successfully created.'
+    # else
+    #   render 'new', alert: 'Error'
+    # end
+  end
+
+  def edit
+  
+  end
+
+  def update
+    @product.update_attributes(product_params)
+  end
+
+  def destroy
+    @product.destroy
+  end
+
+  private
+
+  def find_product
+    @product = Product.find(params[:id])
+  end
+
+  def product_params
+    params.require(:product).permit(:name, :product_type, :product_cycle, :quantity, :unit_measurement, :store, :price, :unit_price, :purchase_date, :user_id)
+  end
+
 end
+
+
+
+
+
+
+
+
+
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,4 @@
 class Product < ActiveRecord::Base
+  enum product_cycle: [ :to_buy, :stock, :no_more ]
   belongs_to :user
 end
