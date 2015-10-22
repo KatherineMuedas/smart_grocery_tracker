84a0daaec559c3606f2cdb0643698494466d7e2b
katherine <muedasnka@gmail.com>
2015-10-21 20:31:09 -0600

    adds a fridge table

--- app/controllers/fridges_controller.rb
+++ app/controllers/fridges_controller.rb
@@ -1,10 +1,46 @@
 class FridgesController < ApplicationController
   include SmartListing::Helper::ControllerExtensions
   helper  SmartListing::Helper
+  before_filter :find_product, except: [:index, :new, :create]
   def index
-    
+    products_scope = Product.where(product_cycle: 1)
+    products_scope = products_scope.where('name ilike ?', "%#{params[:filter]}%") if params[:filter]
+    @products = smart_listing_create(:products_stock, products_scope, partial: "fridges/products_stock_list", default_sort: {name: "asc"})
+  end
+  def new
+    @product = Product.new
   end
   
+  def create
+    @product = Product.create(product_params)
+    # if @product.save
+    #   redirect_to products_path, notice: 'Successfully created.'
+    # else
+    #   render 'new', alert: 'Error'
+    # end
+  end
+
+  def edit
+  
+  end
+
+  def update
+    @product.update_attributes(product_params)
+  end
+
+  def destroy
+    @product.destroy
+  end
+
+  private
+
+  def find_product
+    @product = Product.find(params[:id])
+  end
+
+  def product_params
+    params.require(:product).permit(:name, :product_type, :product_cycle, :quantity, :unit_measurement, :store, :price, :unit_price, :purchase_date, :user_id)
+  end
 end
 
 
