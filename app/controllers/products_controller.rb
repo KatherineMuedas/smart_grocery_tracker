dbe83b5bd52b2f4e4b7f5ef594e16595b2a8341c
katherine <muedasnka@gmail.com>
2015-10-25 19:28:58 -0600

    transfer products from grocery list to personal inventory

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,11 +2,14 @@ class ProductsController < ApplicationController
   include SmartListing::Helper::ControllerExtensions
   helper  SmartListing::Helper
   
-  before_filter :find_product, except: [:index, :new, :create]
+  before_filter :find_product, only: [:edit, :update, :destroy]
   def index
+    session[:selected_products] = {} if params[:clear]
     products_scope = Product.where(product_cycle: 0)
     products_scope = products_scope.where('name ilike ?', "%#{params[:filter]}%") if params[:filter]
     @products = smart_listing_create(:products_to_buy, products_scope, partial: "products/products_to_buy_list", default_sort: {name: "asc"})
+    
+    set_selected_products
   end
 
   def new
@@ -34,6 +37,32 @@ class ProductsController < ApplicationController
     @product.destroy
   end
 
+  def move_to_inventory
+    if params[:id]
+      @products_to_buy = Product.where(id: params[:id])
+    else
+      @products_to_buy = Product.where(id: session[:selected_products].try(:keys))
+      session[:selected_products] = {}
+    end
+
+    @products_to_buy.update_all(product_cycle: 1)
+
+    set_selected_products
+  end
+
+  def select_products
+    params[:product_selection].each do |key, value|
+      if value == "true"
+        session[:selected_products] ||= {}
+        session[:selected_products][key] = 1
+      else
+        session[:selected_products].delete(key)
+      end
+    end
+
+    set_selected_products
+  end
+
   private
 
   def find_product
@@ -42,6 +71,10 @@ class ProductsController < ApplicationController
 
   def product_params
     params.require(:product).permit(:name, :product_type, :product_cycle, :quantity, :unit_measurement, :store, :price, :unit_price, :purchase_date, :user_id)
+  end
+
+  def set_selected_products
+    @selected_products = Product.where(product_cycle: 0).where(id: session[:selected_products].try(:keys))
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   
   devise_for :users
+  post '/products/select_products', to: "products#select_products", as: :select_products
+  put '/products/move_to_inventory', to: "products#move_to_inventory", as: :move_to_inventory
+  post '/fridges/select_fridges', to: "fridges#select_fridges", as: :select_fridges
+  put '/fridges/move_to_list', to: "fridges#move_to_list", as: :move_to_list
   resources :products
   resources :fridges 
   root to: 'products#index'
