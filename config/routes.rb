66a6eda199435fd66ff5858d9d0e1461bb3fa54b
katherine <muedasnka@gmail.com>
2015-10-25 20:10:41 -0600

    transfer products from inventory to grocery list

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   post '/products/select_products', to: "products#select_products", as: :select_products
   put '/products/move_to_inventory', to: "products#move_to_inventory", as: :move_to_inventory
-  post '/fridges/select_fridges', to: "fridges#select_fridges", as: :select_fridges
+  post '/fridges/select_products_to_list', to: "fridges#select_products_to_list", as: :select_products_to_list
   put '/fridges/move_to_list', to: "fridges#move_to_list", as: :move_to_list
   resources :products
   resources :fridges 
