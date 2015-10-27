d9e4c1bda174a7d7b9cce918eb5cea11f59f5d59
katherine <muedasnka@gmail.com>
2015-10-27 00:01:49 -0600

    include_blank to form

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ActiveRecord::Base
   enum product_cycle: [ :to_buy, :stock, :no_more ]
   belongs_to :user
   PRODUCT_CATEGORIES = ["Vegetables" , "Fruits", "Frozen" , "Meat" , "Seafood" , "Herbs","Soups", "Breads", "Beverages", "Dairy", "Pantry"]
-  PRODUCT_MEASUREMENTS = ["tbs" , "oz", "cups" , "lbs" ,"liters", "grams" "packages" , "bottles"]
+  PRODUCT_MEASUREMENTS = ["tbs" , "oz", "cups" , "lbs" ,"liters", "grams" "packages" , "bottles", "items"]
 
   validates :name, :product_type, :store, :purchase_date, :expiration_date, presence: true
 end
